761c5d09fb3be2bee3b602df6fbbde4a48c5beb6
moreau <yorik-ba-56@hotmail.Fr>
2021-05-03 03:32:43 +0200

    commit n°3 - Ajout des photos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,9 @@
 <!--            <img class="card-img-top" src="..." alt="Card image cap">-->
             <div class="card-body">
               <h5 class="card-title">{{data.titre}}</h5>
+              <img :src="require(`../assets/Categories/${data.titre}/Cat_${data.titre}.jpg`)" :alt="data.titre">
+<!--              <img :src="require(`../assets/Categories/Chambre/Cat_Chambre.jpg`)" :alt="data.titre">-->
+<!--              src/assets/Categories/Chambre/Cat_Chambre.jpg-->
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <router-link class="btn btn-primary" :to="{ name: 'sous_categorie', params: { idC: data.id }}">Aller voir </router-link>
             </div>
@@ -26,9 +29,14 @@ export default {
   name: 'Home',
   data(){
     return{
-      myJson: json
+      myJson: json,
+
     }
+  },
+  methods: {
+
   }
+
 
 }
 </script>
